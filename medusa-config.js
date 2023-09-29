cdd9426293c3ea89d161f7954e00378268b67a8d
Davide Zeni <43388491+zenosaaur@users.noreply.github.com>
2023-09-29 17:13:19 +0200

    auto-reload false

--- medusa-config.js
+++ medusa-config.js
@@ -46,7 +46,7 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
-      autoRebuild: true,
+      autoRebuild: false,
       develop: {
         open: process.env.OPEN_BROWSER !== "false",
       },
